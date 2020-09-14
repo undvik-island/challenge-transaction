8154c30e4b7bc4035ded1898ff8a232da4f4f84e
undvik <igorundvik@outlook.com>
2020-09-14 12:09:29 -0300

    removendo dist

--- dist/src/models/Transaction.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Transaction = /** @class */ (function () {
-    function Transaction() {
-    }
-    return Transaction;
-}());
-exports.default = Transaction;
